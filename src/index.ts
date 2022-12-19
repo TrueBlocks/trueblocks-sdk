1f7a0407426ff08903edbb2492942cb48df14f2e
tjayrush <jrush@quickblocks.io>
2022-12-19 11:11:06 -0500

    Cleaning up package.json and index

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export * from './lib/api_callers';
 export * from './paths';
 export * from './types';
 export function getVersion(): string {
-    return "TrueBlocks SDK v0.45.12";
+  return 'TrueBlocks SDK v0.45.0';
 }
