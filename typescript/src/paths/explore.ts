8a48b7aba6b2ec1c440965d3651b9cad8ced21c4
tjayrush <jrush@quickblocks.io>
2022-12-31 10:51:05 -0500

    Removes unused code

--- typescript/src/paths/explore.ts
+++
@@ -1,8 +0,0 @@
-/* eslint object-curly-newline: ["error", "never"] */
-/* eslint max-len: ["error", 160] */
-/*
- * This file was generated with makeClass --sdk. Do not edit it.
- */
-//
-// The explore tool is not available in the SDK.
-//
