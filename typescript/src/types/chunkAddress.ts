66dd7ff529c620e0c3ee1684c03b46a7919c71af
tjayrush <jrush@quickblocks.io>
2023-04-07 05:36:56 -0400

    Catching up to core

---
+++ typescript/src/types/chunkAddress.ts
@@ -0,0 +1,13 @@
+/* eslint object-curly-newline: ["error", "never"] */
+/* eslint max-len: ["error", 160] */
+/*
+ * This file was generated with makeClass --sdk. Do not edit it.
+ */
+import { address, blockRange, uint64 } from '.';
+
+export type ChunkAddress = {
+  address: address
+  range: blockRange
+  offset: uint64
+  count: uint64
+}
