d347f4acf12d51432938166bd57d1bb98823ec62
Thomas Jay Rush <jrush@quickblocks.io>
2025-03-18 21:46:43 -0400

    Transfers now have all fields needed for full accounting.

--- typescript/src/types/transfer.ts
+++ typescript/src/types/transfer.ts
@@ -8,14 +8,29 @@
  * This file was auto generated. DO NOT EDIT.
  */
 
-import { address, blknum, int256, lognum, txnum, uint64 } from '.';
+import { address, blknum, int256, Log, lognum, Transaction, txnum, uint64 } from '.';
 
 export type Transfer = {
-  amount: int256;
+  amountIn?: int256;
+  amountOut?: int256;
   asset: address;
   blockNumber: blknum;
   decimals: uint64;
+  gasOut?: int256;
   holder: address;
+  internalIn?: int256;
+  internalOut?: int256;
+  log?: Log;
   logIndex: lognum;
+  minerBaseRewardIn?: int256;
+  minerNephewRewardIn?: int256;
+  minerTxFeeIn?: int256;
+  minerUncleRewardIn?: int256;
+  prefundIn?: int256;
+  recipient: address;
+  selfDestructIn?: int256;
+  selfDestructOut?: int256;
+  sender: address;
+  transaction?: Transaction;
   transactionIndex: txnum;
 };
