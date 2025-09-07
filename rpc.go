62fdfe423665a9225e48a3407c9d79f90a67b52b
Thomas Jay Rush <jrush@quickblocks.io>
2025-09-07 11:26:42 -0700

    Improves rpc.PingRpc

--- rpc.go
+++ rpc.go
@@ -2,6 +2,7 @@ package sdk
 
 import "github.com/TrueBlocks/trueblocks-core/src/apps/chifra/pkg/rpc"
 
-func PingRpc(providerUrl string) error {
-	return rpc.PingRpc(providerUrl)
+func PingRpc(providerUrl string) (*rpc.PingResult, error) {
+	result, err := rpc.PingRpc(providerUrl)
+	return result, err
 }
