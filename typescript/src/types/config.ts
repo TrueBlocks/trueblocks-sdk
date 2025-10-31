7793abdcab3d8ec4992e592f68245215c9e8f500
Thomas Jay Rush <jrush@quickblocks.io>
2025-10-31 00:58:42 -0400

    Cleans

--- typescript/src/types/config.ts
+++ typescript/src/types/config.ts
@@ -4,8 +4,6 @@
  * Copyright 2016, 2024 The TrueBlocks Authors. All rights reserved.
  * Use of this source code is governed by a license that can
  * be found in the LICENSE file.
- *
- * This file was auto generated. DO NOT EDIT.
  */
 
 import { CacheItem, Chain } from '.';
