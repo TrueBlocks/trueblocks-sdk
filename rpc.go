8dfd0eaa8bb6ed3e8b1aad2049616ddb8b8a433c
Thomas Jay Rush <jrush@quickblocks.io>
2025-11-28 20:53:05 -0500

    Adds EstimateGasAndPrice to SDK

--- rpc.go
+++ rpc.go
@@ -1,8 +1,16 @@
 package sdk
 
-import "github.com/TrueBlocks/trueblocks-chifra/v6/pkg/rpc"
+import (
+	"github.com/TrueBlocks/trueblocks-chifra/v6/pkg/base"
+	"github.com/TrueBlocks/trueblocks-chifra/v6/pkg/rpc"
+)
 
 func PingRpc(providerUrl string) (*rpc.PingResult, error) {
 	result, err := rpc.PingRpc(providerUrl)
 	return result, err
+}
+
+// EstimateGasAndPrice estimates gas and retrieves current gas price in a single batch RPC call
+func EstimateGasAndPrice(chain string, from base.Address, to base.Address, data string, value *base.Wei) (estimatedGas base.Gas, gasPrice base.Gas, err error) {
+	return rpc.EstimateGasAndPrice(chain, from, to, data, value)
 }
